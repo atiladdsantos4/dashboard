94766db8a64200de745b991d4911cdbddfdc5736
atiladdsantos <atiladdsantos4@gmail.com>
2025-05-12 22:19:45 -0300

    rest of code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+//--> Função retorna um elemento customizado <--//
+function App() {
+  return (
+    <div className="App">
+       <h1>Hello World!</h1>
+     </div>
+  );
+}
+
+export default App;
